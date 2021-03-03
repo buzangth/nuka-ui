9e69e3f5c6f5871cc05e20af30d2d8ef88eee040
buzangth <thbusang@outlook.com>
2021-03-03 15:54:15 +0200

    sign in page buttons plus padding

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -7,6 +7,74 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Nuka'),
       ),
+      body: _buildContent(),
     );
   }
+}
+
+Widget _buildContent() {
+  return Padding(
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Text(
+          'Sign In',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        RaisedButton(
+          child: Text('Sign in with Google'),
+          onPressed: () {
+            print("passed google");
+          },
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        RaisedButton(
+          child: Text('Sign in with Facebook'),
+          onPressed: () {
+            print("passed facebook");
+          },
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        RaisedButton(
+          child: Text('Sign in with Email'),
+          onPressed: () {
+            print("passed email");
+          },
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          'Or',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 12.00,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        RaisedButton(
+          child: Text('Sign in with Announimously'),
+          onPressed: () {
+            print("passed announse");
+          },
+        ),
+      ],
+    ),
+  );
 }
