2f258629ad8f4d20c86a58a7ebec5be639afb92b
buzangth <thbusang@outlook.com>
2021-03-03 13:48:39 +0200

    sign_in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nuka_ui/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'nuka',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: SignInPage(),
+    );
   }
 }
