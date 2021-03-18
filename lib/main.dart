fa77fa1936688d43f16c6f9d069393fe515ee232
buzangth <thbusang@outlook.com>
2021-03-18 11:59:39 +0200

    initializing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nuka_ui/app/sign_in/sign_in_page.dart';
 
 void main() {
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
